b28ce730692480af465de844326b597618c85bf2
Nguyenpham1008 <nguyenpq.it@gmail.com>
2017-02-28 10:27:42 +0700

    up

---
+++ HelloAppEngine/src/com/nguyenpham/HelloAppEngineServlet.java
@@ -0,0 +1,12 @@
+package com.nguyenpham;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class HelloAppEngineServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello Every Body");
+	}
+}
